5e25ad0f08918762088f00c7fb15062ea909c589
Joel da Rosa <webjoel@users.noreply.github.com>
2015-06-11 09:34:56 -0300

    Update ws.js

--- ws.js
+++ ws.js
@@ -77,12 +77,15 @@ app.get('/ws/:mac', autenticacao, function (req, res) {
     	// TODO RECUPERAR DE UMA BASE DE DADOS
     
 	var result = JSON.parse('{ "clientes" : [ '
-                            + '{"codigo":"1","nome":"Cliente Teste","cnpj":"01.234.456/0001-23","telefone":"(00)0000-0000","contato":"Contato 1","email":"teste@teste.com","logradouro":"Rua Geral","numero":"1","bairro":"Centro","cidade":"Pequenópolis","estado":"DF","tipo":"Especial","limite":1000,"venda":0,"valor_titulos_vencidos":0,"quantidade_titulos_vencidos":0} ],'
+                            + '{"codigo":"1","nome":"Cliente Teste 1","cnpj":"01.234.456/0001-23","telefone":"(00)0000-0000","contato":"Contato 1","email":"teste1@teste.com","logradouro":"Rua Geral 1","numero":"1","bairro":"Centro","cidade":"Pequenópolis","estado":"DF","tipo":"Especial","limite":1000,"venda":0,"valor_titulos_vencidos":0,"quantidade_titulos_vencidos":0}, '
+                            + '{"codigo":"2","nome":"Cliente Teste 2","cnpj":"98.765.432/0001-45","telefone":"(00)0000-0000","contato":"Contato 2","email":"teste2@teste.com","logradouro":"Rua Geral 2","numero":"2","bairro":"Centro","cidade":"Pequenópolis","estado":"DF","tipo":"SPC","limite":500,"venda":0,"valor_titulos_vencidos":0,"quantidade_titulos_vencidos":0} ],'
                             + '"condicoes" : ['
-                            + '{"codigo":"1","descricao":"A VISTA","desconto_maximo":20} ],'
+                            + '{"codigo":"1","descricao":"A VISTA","desconto_maximo":20,"considera_limite":"N"}, '
+                            + '{"codigo":"2","descricao":"A PRAZO","desconto_maximo":10,"considera_limite":"S"} ],'
                             + '"produtos" : ['
-                            + '{"codigo":"1","descricao":"Produto Teste","unidade":"UN","referencia":"001","fornecedor":"Fornecedor 1","quantidade_estoque":100,"percentual_comissao":7,"percentual_desconto":10,"valor_minimo":6.23,"valor_venda":6.92} ],'
-                            + '"parametros" : {"nome_empresa":"EMPRESA TESTE","controlar_limite_cliente":"S","controlar_desconto_cond_pagto":"N","controlar_estoque":"S","valor_saldo_flex": 50.0} }');
+                            + '{"codigo":"1","descricao":"Produto Teste 1","unidade":"UN","referencia":"001","fornecedor":"Fornecedor 1","quantidade_estoque":100,"percentual_comissao":7,"percentual_desconto":10,"valor_minimo":6.23,"valor_venda":6.92}, '
+                            + '{"codigo":"2","descricao":"Produto Teste 2","unidade":"KG","referencia":"002","fornecedor":"Fornecedor 2","quantidade_estoque":50,"percentual_comissao":7,"percentual_desconto":15,"valor_minimo":22.01,"valor_venda":25.89} ],'
+                            + '"parametros" : {"nome_empresa":"EMPRESA TESTE","controlar_limite_cliente":"S","controlar_desconto_cond_pagto":"S","controlar_estoque":"S","valor_saldo_flex": 50.0} }');
 
 	res.status(200).send(result);
 });
